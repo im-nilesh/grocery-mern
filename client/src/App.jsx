7e13fab0e79be3e3fcb8acfde9a2b96ffa1dca5d
Nilesh <nileshdadab@gmail.com>
2025-04-26 12:00:52 +0530

    Best Seller component and product card component created with add to cart function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -8,6 +9,8 @@ const App = () => {
   return (
     <div>
       {isSellerPath ? null : <NavBar />}
+
+      <Toaster />
       <div
         className={`${
           isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32 py-4"
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,37 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
+import { dummyProducts } from "../assets/assets";
+import { toast } from "react-hot-toast";
 
 export const AppContextProvider = ({ children }) => {
+  const currency = "₹";
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setcartItems] = useState({});
+
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  const addToCart = () => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+    setcartItems(cartData);
+    toast.success("Added to cart");
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   const value = {
     navigate,
@@ -16,6 +41,9 @@ export const AppContextProvider = ({ children }) => {
     setIsSeller,
     showUserLogin,
     setShowUserLogin,
+    products,
+    currency,
+    addToCart,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
