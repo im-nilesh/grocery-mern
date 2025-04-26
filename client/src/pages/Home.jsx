692e871bd2114636fe16e3966b3882c4f7b3ee7e
Nilesh <nileshdadab@gmail.com>
2025-04-26 11:33:22 +0530

    Categories Done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainBanner from "../components/MainBanner";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
     <div>
       <MainBanner />
+      <Categories />
     </div>
   );
 };
