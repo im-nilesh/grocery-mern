70a5a9b8959a3ac58f955d1025bea77ffb6f1e0e
Nilesh <nileshdadab@gmail.com>
2025-04-27 11:25:26 +0530

    Home Page Fully Completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-        Copyright 2025 © PrebuiltUI All Right Reserved.
+        Copyright {new Date().getFullYear()} © Nilesh D All Right Reserved.
       </p>
     </div>
   );
