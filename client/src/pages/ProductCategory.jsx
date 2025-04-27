5a28b4058fa8e3a492fb7f908b9d760a32f090b6
Nilesh <nileshdadab@gmail.com>
2025-04-27 12:56:00 +0530

    Product Category Complete

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAppContext } from "../context/AppContext";
 import { useParams } from "react-router-dom";
 import { categories } from "../assets/assets";
+import ProductCard from "../components/ProductCard";
 
 const ProductCategory = () => {
   const { products } = useAppContext();
@@ -18,10 +19,25 @@ const ProductCategory = () => {
     <div className="mt-16">
       {searchCategory && (
         <div className="flex flex-col items-end w-max">
-          <p className="text-2x1 font-medium">
+          <p className="text-2xl font-medium ">
             {searchCategory.text.toUpperCase()}
           </p>
-          <div className="w-16 g-0.5 bg-primary rounded-full"></div>
+          <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+        </div>
+      )}
+
+      {filteredProducts.length > 0 ? (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 ">
+          {filteredProducts.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-[60vh]">
+          <p className="text-2xl font-medium text-gray-500">
+            No products found in this category. Please check back later or
+            explore other categories.
+          </p>
         </div>
       )}
     </div>
