cd6b3eb16d7c0ca999897ab2c8aadf42be2cbf1b
Nilesh <nileshdadab@gmail.com>
2025-05-01 13:11:49 +0530

    API Working correctly and user is being created

--- server/server.js
+++ server/server.js
@@ -8,17 +8,19 @@ import userRouter from "./routes/userRoute.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
-await connectDB();
+(async () => {
+  await connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+  const allowedOrigins = ["http://localhost:5173"];
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-app.get("/", (req, res) => res.send("API is Working"));
-app.use("/api/user", userRouter);
+  app.get("/", (req, res) => res.send("API is Working"));
+  app.use("/api/user", userRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+})();
