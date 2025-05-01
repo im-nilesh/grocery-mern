fc96c76b32d4398d8b8812ddf15ade0a9b5db938
Nilesh <nileshdadab@gmail.com>
2025-05-01 12:50:12 +0530

    User Controller created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,44 @@
-export const register = async (req, res) => {};
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken;";
+
+//api/user/register
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.json({ success: false, message: "User Already Exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token".token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Use Secure Cookies in production
+      sameSite: process.env.Node_ENV === "production" ? "none" : "strict", // CSRF protection
+      maxAge: 7 * 24 * 60 * 1000,
+    });
+
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
