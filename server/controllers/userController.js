f6aa9791c049f292babc14426ec00f6884640318
Nilesh <nileshdadab@gmail.com>
2025-05-01 13:33:06 +0530

    Login API fully working

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,6 +11,7 @@ export const register = async (req, res) => {
     if (!name || !email || !password) {
       return res.json({ success: false, message: "Missing Details" });
     }
+
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.json({ success: false, message: "User Already Exists" });
@@ -23,11 +24,11 @@ export const register = async (req, res) => {
       expiresIn: "7d",
     });
 
-    res.cookie("token".token, {
+    res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Use Secure Cookies in production
-      sameSite: process.env.Node_ENV === "production" ? "none" : "strict", // CSRF protection
-      maxAge: 7 * 24 * 60 * 1000,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     return res.json({
@@ -35,10 +36,48 @@ export const register = async (req, res) => {
       user: { email: user.email, name: user.name },
     });
   } catch (error) {
-    console.log(error.message);
-    res.json({
-      success: false,
-      message: error.message,
+    console.error(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "Invalid Email or Password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid Email or Password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
     });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: error.message });
   }
 };
