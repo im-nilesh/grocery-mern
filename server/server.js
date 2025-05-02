a508fdbf218e13d4fab68a424df5f2a45bcebab7
Nilesh <nileshdadab@gmail.com>
2025-05-02 17:10:44 +0530

    cloudinary api key and secret key done

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ import connectDB from "./configs/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 (async () => {
   await connectDB();
+  await connectCloudinary();
 
   const allowedOrigins = ["http://localhost:5173"];
 
