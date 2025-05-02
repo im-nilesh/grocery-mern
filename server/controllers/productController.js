7ee8c820c2fe9ac004af0c7d7559c88aa00e45d3
Nilesh <nileshdadab@gmail.com>
2025-05-02 16:57:18 +0530

    Product controller and modules created

---
+++ server/controllers/productController.js
@@ -0,0 +1,8 @@
+// add product : /api/product/add
+export const addProduct = async (req, res) => {};
+// get product : /api/product/list
+export const productList = async (req, res) => {};
+// get single product : /api/product/id
+export const productById = async (req, res) => {};
+// change stock : /api/product/stock
+export const changeStock = async (req, res) => {};
