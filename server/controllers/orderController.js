d2e9d4e51d5bc61247653740317e0698d9f50ad1
Nilesh <nileshdadab@gmail.com>
2025-05-11 15:46:34 +0530

    Implement online payment processing with Stripe: add placeOrderStripe controller and update order routes

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,6 +1,6 @@
 import Product from "../models/Product.js";
 import Order from "../models/Order.js";
-
+import stripe from "stripe";
 //place Order COD : /api/order/cod
 
 export const placeOrderCOD = async (req, res) => {
@@ -70,5 +70,71 @@ export const getAllOrders = async (req, res) => {
     });
   } catch (error) {
     res.json({ success: false, message: error.message });
+  }
+};
+
+// place order stripe : /api/order/stripe
+export const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, address } = req.body;
+    const { origin } = req.headers;
+
+    if (!address || items.length === 0) {
+      return res.json({ success: false, message: "Invalid date" });
+    }
+    let productData = [];
+    //calc amount using items
+
+    let amount = await items.reduce(async (acc, items) => {
+      const product = await Product.findById(items.product);
+      productData.push({
+        name: product.name,
+        price: product.offerPrice,
+        quantity: items.quantity,
+      });
+      return (await acc) + product.offerPrice * items.quantity;
+    }, 0);
+
+    //add tax charge (2%)
+
+    amount += Math.floor(amount * 0.02);
+
+    const order = await Order.create({
+      userId,
+      items,
+      amount,
+      address,
+      paymentType: "Online",
+    });
+
+    //stripe gateway initialization
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+    const line_items = productData.map((item) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: Math.floor(item.price + item.price * 0.02) * 100, // Adding tax charge (2%)
+      },
+      quantity: item.quantity,
+    }));
+
+    //create session
+    const session = await stripeInstance.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      success_url: `${origin}/loader?next=my-orders`,
+      cancel_url: `${origin}/cart`,
+      metadata: {
+        orderId: order._id.toString(),
+        userId,
+      },
+    });
+
+    return res.json({ success: true, url: session.url });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
   }
 };
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -3,12 +3,14 @@ import {
   getUserOrders,
   getAllOrders,
   placeOrderCOD,
+  placeOrderStripe,
 } from "../controllers/orderController.js";
 import authUser from "../middlewares/authUser.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/cod", authUser, placeOrderCOD);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
 orderRouter.get("/user", authUser, getUserOrders);
 orderRouter.get("/seller", authUser, getAllOrders);
 
