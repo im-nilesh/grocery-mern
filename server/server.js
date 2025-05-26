dde7988442e850fe22a73200aa3e8f973f40a1b5
Nilesh <nileshdadab@gmail.com>
2025-05-26 19:00:20 +0530

    Update CORS configuration to allow requests from all origins

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 4000;
   await connectDB();
   await connectCloudinary();
 
-  const allowedOrigins = ["http://localhost:5173"];
+  const allowedOrigins = ["*"];
 
   app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhook);
 
